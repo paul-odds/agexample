2181de0d2206c47fc1814b37a41cf953ed137581
Patchara Boonrung <paul@odds.team>
2023-11-25 11:36:12 +0700

    Refactor error content: extract interface

--- src/app/shared/modal-error/model/error-content.model.ts
+++ src/app/shared/modal-error/model/error-content.model.ts
@@ -1,8 +1,12 @@
-export class ErrorContent {
+interface IErrorContent {
+  title: string;
+  messages: string[];
+}
+export class ErrorContent implements IErrorContent {
   title: string;
   messages: string[];
 
-  constructor(errorContent?: Partial<ErrorContent>) {
+  constructor(errorContent?: Partial<IErrorContent>) {
     this.title = errorContent?.title ?? '';
     this.messages = errorContent?.messages ?? [];
   }
